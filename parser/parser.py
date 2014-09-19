3d5079cb11a50ac321ca2c128ed608cd864beb74
Carlos Oliveira <carlospecter@gmail.com>
2014-09-19 13:06:02 -0300

    Persistindo parlamentares no banco de dados
    Signed-off-by: Carlos Oliveira <carlospecter@gmail.com>
    Signed-off-by: José Pedro <1jpsneto@gmail.com>

--- parser/parser.py
+++ parser/parser.py
@@ -2,9 +2,25 @@
 
 from urllib2 import urlopen, HTTPError
 import unicodedata
-# import os
+import subprocess
+import sys
 from time import sleep
 import xml.etree.ElementTree as ET
+import MySQLdb
+
+db_user = str(sys.argv[1])
+try:
+    db_pwd = str(sys.argv[2])
+except IndexError:
+    db_pwd = ''
+
+if db_pwd != '':
+    create_db_string = "mysql -u %s -p%s -e 'CREATE DATABASE IF NOT EXISTS espelho_politico'" % (db_user, db_pwd)
+else:
+    create_db_string = "mysql -u %s -e 'CREATE DATABASE IF NOT EXISTS espelho_politico'" % (db_user)
+
+db = MySQLdb.connect("localhost", db_user, db_pwd, "espelho_politico")
+cursor = db.cursor()
 
 # Classe para parlamentar
 class Parlamentar():
@@ -43,24 +59,55 @@ def remove_acentos(nome):
         # Caso haja acentos, remove-os
         return unicodedata.normalize('NFKD', nome).encode('ASCII', 'ignore')
 
+print "Criando base de dados espelho_politico..."
+sleep(1)
+subprocess.call(create_db_string, shell=True)
+
 print "Obtendo os dados dos parlamentares"
 url_parlamentares = 'http://www.camara.gov.br/SitCamaraWS/Deputados.asmx/ObterDeputados'
 xml_parlamentares = ET.parse(urlopen(url_parlamentares))
 xml_parlamentares = xml_parlamentares.getroot()
 parlamentares = []
+drop_table_string = "DROP TABLE IF EXISTS parlamentar;"
+cursor.execute(drop_table_string)
+db.commit()
+create_table_string = """
+CREATE TABLE parlamentar (
+id int not null primary key,
+nome varchar(255),
+matricula int,
+condicao varchar(255),
+url_foto varchar(400),
+uf varchar(2),
+partido varchar(20),
+telefone varchar(20),
+email varchar(100),
+gabinete int
+);"""
+cursor.execute(create_table_string)
+db.commit()
 for xml_parlamentar in xml_parlamentares:
     parlamentar = Parlamentar()
     parlamentar.nome = xml_parlamentar.find('nomeParlamentar').text
-    parlamentar.id_cadastro = xml_parlamentar.find('ideCadastro').text
-    parlamentar.matricula = xml_parlamentar.find('matricula').text
+    parlamentar.id_cadastro = int(xml_parlamentar.find('ideCadastro').text)
+    parlamentar.matricula = int(xml_parlamentar.find('matricula').text)
     parlamentar.condicao = xml_parlamentar.find('condicao').text
     parlamentar.url_foto = xml_parlamentar.find('urlFoto').text
     parlamentar.uf = xml_parlamentar.find('uf').text
     parlamentar.partido = xml_parlamentar.find('partido').text
     parlamentar.telefone = xml_parlamentar.find('fone').text
     parlamentar.email = xml_parlamentar.find('email').text
-    parlamentar.gabinete = xml_parlamentar.find('gabinete').text
+    parlamentar.gabinete = int(xml_parlamentar.find('gabinete').text)
+    insert_parlamentar_string = """
+    insert into parlamentar
+    (id, nome, matricula, condicao, url_foto, uf, partido, telefone, email, gabinete)
+    values ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')
+    """ % (parlamentar.id_cadastro, parlamentar.nome, parlamentar.matricula, parlamentar.condicao,
+           parlamentar.url_foto, parlamentar.uf, parlamentar.partido, parlamentar.telefone,
+           parlamentar.email, parlamentar.gabinete)
 
+    cursor.execute(insert_parlamentar_string)
+    db.commit()
     print "Parlamentar", parlamentar.nome, "encontrad@"
     print
     parlamentares.append(parlamentar)
@@ -107,4 +154,4 @@ for parlamentar in parlamentares:
         print
         sleep(1)
 
-
+db.close()
