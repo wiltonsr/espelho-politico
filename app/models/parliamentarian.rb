f2fc10de5e4463cdf0b79957a3f7d91c827575e3
Matheus + Jose Pedro <leitematheus7@hotmail.com>
2014-10-06 15:00:09 -0300

    Criando relacionamento entre proposicao e parlamentar

--- app/models/parliamentarian.rb
+++ app/models/parliamentarian.rb
@@ -1,3 +1,4 @@
 class Parliamentarian < ActiveRecord::Base
+  has_and_belongs_to_many :propositions
   validates :id, presence: true, length: {maximum: 11}
 end
--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,3 +1,4 @@
 class Theme < ActiveRecord::Base
   has_and_belongs_to_many :propositions
+  has_and_belongs_to_many :parliamentarians
 end
