25d40ecd19c6090d27ca7ca686ff202346293933
Matheus Leite + Jose Pedro <leitematheus7@hotmail.com>
2014-10-06 19:39:01 -0300

    Criado método de ordenação de parlamentares de acordo com a quantidade de proposições em um determinado tema

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -2,7 +2,16 @@ class RankingsController < ApplicationController
   def index
   	@themes = Theme.all
   	@themes = order_themes(@themes)
-    @parliamentarian = Parliamentarian.all
+  
+
+  @themes.each do |theme|
+    propositions = theme.propositions.to_a
+    propositions.sort! {|a,b| a.parliamentarians[0].propositions.count <=> b.parliamentarians[0].propositions.count}
+    puts "=="*100
+    puts theme.propositions[0].parliamentarians[0].propositions.count
+    puts "=="*100
+    theme.propositions = propositions
+    end
   end   
 
   def order_themes(themes)
