3f97ee5512b007e664f332a916035b9adfa05cee
Matheus Leite <matheus.leite27@gmail.com>
2014-10-08 10:30:32 -0300

    Removendo migrations que não são mais necessárias

--- db/migrate/20140927170251_create_propositions_parliamentarians.rb
+++
@@ -1,8 +0,0 @@
-class CreatePropositionsParliamentarians < ActiveRecord::Migration
-  def change
-    create_table :propositions_parliamentarians do |t|
-      t.belongs_to :propositions
-      t.belongs_to :parliamentarians
-    end
-  end
-end
