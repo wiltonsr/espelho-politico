45fdcb767126fe9d645a2a939e36c481c44dd635
Eduardo Vital <vitaldu@gmail.com>
2014-10-10 02:58:47 -0300

    Usando internacionalização para traduzir mensagens de erro da model usuário.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,18 +4,14 @@ class User < ActiveRecord::Base
 	before_create :create_activation_digest
 	has_secure_password
 
-	validates_presence_of :name, :email, :username, :password, message: 'Campo obrigatório'
+	validates_presence_of :name, :email, :username, :password
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates_format_of :email, with: VALID_EMAIL_REGEX, :on => [:create, :update], message: 'Formato de email inválido'
+	validates_format_of :email, with: VALID_EMAIL_REGEX, :on => [:create, :update]
 	VALID_USERNAME_REGEX = /[a-z0-9._-]{5,30}/i
-	validates_format_of :username, with: VALID_USERNAME_REGEX, :on  => [:create, :update], message: 'Formato inválido'
-	validates_length_of :email, maximum: 30, mesage: 'Digite um email com menos de 30 caracteres'
-	validates_length_of :username, within: 5..20,
-		too_long: 'Digite um nome de usuário menor que 20 caracteres',
-		too_short: 'Digite um nome de usuário maior que 5 caracteres'
-	validates_length_of :password, within: 5..20,
-		too_long: 'Digite um nome de usuário menor que 20 caracteres',
-		too_short: 'Digite um nome de usuário maior que 5 caracteres'
+	validates_format_of :username, with: VALID_USERNAME_REGEX, :on  => [:create, :update]
+	validates_length_of :email, maximum: 30
+	validates_length_of :username, within: 5..20
+	validates_length_of :password, within: 5..20
 	validates_uniqueness_of [:email, :username], case_sensitive: false
 
 	# Retorna o hash da string fornecida.
