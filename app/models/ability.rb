fc8f7f140a623b4e87df4cf5f5d163ce91071ae5
Daniel <danieelteles@gmail.com>
2014-10-19 17:05:21 -0200

    CanCanCan Views Adicionando a Gem CanCanCan para validação de acessos e fazendo algumas modificações na view para Mobile.

---
+++ app/models/ability.rb
@@ -0,0 +1,17 @@
+#This class verifys the roles for each user and limits their access.
+
+class Ability
+  include CanCan::Ability
+   def initialize(user)
+    
+    user ||= User.new #guest user
+
+    if user.current_user.admin? :user
+      can :create, Atendimento
+    end
+
+    if user.admin? :admin
+      can :manage, :all
+    end
+  end
+end
