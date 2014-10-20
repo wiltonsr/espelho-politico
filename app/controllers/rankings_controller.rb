00772394cf2ff4a9238efa93e7a0571db15ffd34
Eduardo Vital <vitaldu@gmail.com>
2014-10-20 11:16:25 -0200

    Refatorando e melhorando método para rankear parlamentares por área.

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,42 +1,8 @@
 class RankingsController < ApplicationController
   def index
-    @themes = Theme.all
-    parliamentarians = Parliamentarian.all
-    @themes = order_themes(@themes)
-
-    parliamentarians.each do |parliamentarian|
-      @themes.each do |theme|
-        json_result = propositions_count_from_parliamentarian_per_theme(parliamentarian.id, theme.id)
-      end
-    end
-
-  end
-
-  def propositions_count_from_parliamentarian_per_theme(parliamentarian_id, theme_id)
-    conn = get_raw_connection
-    hash = conn.select_all(
-      "SELECT parliamentarians.id as 'par_id', themes.id as 'the_id', count(*) as 'propositions' FROM `themes` " +
-      "INNER JOIN `propositions_themes` ON `propositions_themes`.`theme_id` = `themes`.`id` " +
-      "INNER JOIN `propositions` ON `propositions`.`id` = `propositions_themes`.`proposition_id` " +
-      "INNER JOIN `parliamentarians` ON `parliamentarians`.`id` = `propositions`.`parliamentarian_id` " +
-      "WHERE (parliamentarians.id = #{parliamentarian_id} and themes.id = #{theme_id}) " +
-      "GROUP BY themes.id"
-      )
-
-    json_result = hash_to_json hash
-    json_result
-  end
-
-  def hash_to_json(hash)
-    hash.to_json
-  end
-
-  def order_parliamentarians(themes)
-    themes.each do |theme|
-    propositions = theme.propositions.to_a
-    propositions.sort! {|b,a| a.parliamentarians[0].propositions.count <=> b.parliamentarians[0].propositions.count}
-    theme.propositions = propositions
-    end
+    @themes = order_themes(Theme.all)
+    @parliamentarians = Parliamentarian.all
+    @propositions = Proposition.all
   end
 
   def order_themes(themes)
