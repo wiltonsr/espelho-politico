2ec6633cf8e0915599718e84d0276cd7b7de1b94
Matheus Leite <leitematheus7@hotmail.com>
2014-10-21 10:33:50 -0200

    Modificando controller para retornar parlamentares de uma procura

--- app/controllers/parliamentarians_controller.rb
+++ app/controllers/parliamentarians_controller.rb
@@ -1,13 +1,14 @@
 class ParliamentariansController < ApplicationController
   def index
-    @parliamentarians = Parliamentarian.all
+    @parliamentarians = Parliamentarian.search(params[:search])
+    #@parliamentarians = Parliamentarian.all
     # @parliamentarians = order_parliamentarians(@parliamentarians)
   end
 
-  def order_parliamentarians(parliamentarians)
-     parliamentarians  = parliamentarians.to_a
-     parliamentarians.sort! {|b,a| a.propositions.count <=> b.propositions.count}
-  end
+  # def order_parliamentarians(parliamentarians)
+  #    parliamentarians  = parliamentarians.to_a
+  #    parliamentarians.sort! {|b,a| a.propositions.count <=> b.propositions.count}
+  # end
 
   def show
     @parliamentarian = Parliamentarian.find(params[:id])
