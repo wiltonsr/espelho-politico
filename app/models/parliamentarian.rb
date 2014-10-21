f7f1792c411cf1d3dbfe8522c981d4ac61854a9c
Matheus Leite <leitematheus7@hotmail.com>
2014-10-21 10:29:35 -0200

    Definindo método para procurar parlamentares 	Criado método para retornar parlamentares com base numa entrada do usuário.

--- app/models/parliamentarian.rb
+++ app/models/parliamentarian.rb
@@ -1,4 +1,12 @@
 class Parliamentarian < ActiveRecord::Base
   has_many :propositions
   validates :id, presence: true, length: {maximum: 11}
+
+  def self.search(search)
+    if search
+      where(['name LIKE ?', "%#{search}%"])
+    else
+      take(600)
+    end
+  end
 end
