f6015e78ee25edbbd77e8af78a281ed76aab345c
Eduardo Vital <vitaldu@gmail.com>
2014-10-24 21:26:47 -0200

    Utilizando JavaScript para recarregar segundo ranking de forma modular.
    Apenas o segundo ranking é atualizado ao invés de toda a página.

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,11 +1,16 @@
 class RankingsController < ApplicationController
   def index
     @themes = order_themes(Theme.all)
-    @selected_theme_id = (params[:theme_id])
+    if !params[:theme_id].nil?
+      theme_description = params[:theme_id].split()
+      theme_description.pop
+      theme_description = theme_description.join(" ")
+      @selected_theme_id = Theme.find_by(description: theme_description).id
+    end
   end
 
   def order_themes(themes)
-		themes  = themes.to_a
-  	themes.sort! {|b,a| a.propositions.count <=> b.propositions.count}
+    themes  = themes.to_a
+    themes.sort! {|b,a| a.propositions.count <=> b.propositions.count}
   end
 end
