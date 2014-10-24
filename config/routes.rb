8d4452f93a635941bd017550239c1bc41d4d7c43
Eduardo Vital <vitaldu@gmail.com>
2014-10-24 21:26:47 -0200

    Utilizando JavaScript para recarregar segundo ranking de forma modular.
    Apenas o segundo ranking é atualizado ao invés de toda a página.

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
   get 'signin' => 'sessions#new'
   post 'signin' => 'sessions#create'
   delete 'signout' => 'sessions#destroy'
-
+  post 'ranking' => 'rankings#index'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
