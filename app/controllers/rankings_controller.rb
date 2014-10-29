a0c780ef82d47ec3ecfb0b0b91eb9781284ae8a5
Eduardo Vital <vitaldu@gmail.com>
2014-10-28 22:26:53 -0200

    Refatorando ranking controller.
    Criando método para instruções que estavam avulsas no index da controller.

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,11 +1,15 @@
 class RankingsController < ApplicationController
   def index
     @themes = order_themes(Theme.all)
+    @selected_theme_id = find_theme_id_by_params(params)
+  end
+
+  def find_theme_id_by_params(params)
     if !params[:theme_id].nil?
       theme_description = params[:theme_id].split()
       theme_description.pop
       theme_description = theme_description.join(" ")
-      @selected_theme_id = Theme.find_by(description: theme_description).id
+      Theme.find_by(description: theme_description).id
     end
   end
 
