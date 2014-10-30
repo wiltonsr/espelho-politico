66d3ee64ee25ae123bf0e448b0799c25e9582488
Wilton Rodrigues <braynwilton@gmail.com>
2014-10-30 14:16:56 -0200

    Incluindo método para validar o parametros para o devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  include SessionsHelper
   def get_raw_connection
     conn = ActiveRecord::Base.connection
     conn
+  end
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:user) {|u| u.permit(:name, :email, :password, :username, :password_confirmation)}
+    devise_parameter_sanitizer.for(:sign_in) {|u| u.permit(:username, :password)}
+    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name, :email, :password, :username, :password_confirmation)}
   end
 end
