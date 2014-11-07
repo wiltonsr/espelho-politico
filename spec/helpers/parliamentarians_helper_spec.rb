b4be15f51fe0112ca4f3af6354591968a502a612
Daniel Teles <danieelteles@gmail.com>
2014-11-07 21:06:42 -0200

    parliamentarians_helper_spec
    Deletando a parliamentarians_helper_spec pois ela não é mais necessária.

--- spec/helpers/parliamentarians_helper_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe ParliamentariansHelper, :type => :helper do
-  it "expects to find all the parlamentarians of one state" do
-    Parliamentarian.where("df") == parlamentarians_by_state("df")
-  end
-end
