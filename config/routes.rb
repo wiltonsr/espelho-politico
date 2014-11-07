18343eddcc3f17ba8022f7a3509deb25ebba16db
Daniel <danieelteles@gmail.com>
2014-11-07 20:40:43 -0200

    Javascript, Routes e Views(método por partido) Adicionando Javascript para chamada do método pelo dropdown, refatorando a routes e fazendo alterações necessárias na view.

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,10 @@ Rails.application.routes.draw do
     get 'parliamentarians_per_state', :on => :collection
   end
 
+  resources :parliamentarians, :except => [:show]  do
+    get 'parliamentarians_per_party', :on => :collection
+  end
+
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
