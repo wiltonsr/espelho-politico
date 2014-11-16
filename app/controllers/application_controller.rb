32118e3334671f422fba4bb0552030d9c6d0e6a3
Daniel <danieelteles@gmail.com>
2014-11-15 22:20:42 -0200

    Testes Refatorando testes do ranking, adicionando nocov em determinados pontos do código e deletando várias partes inúteis do código.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+# :nocov:
   def get_raw_connection
     conn = ActiveRecord::Base.connection
     conn
@@ -16,14 +15,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_in) {|u| u.permit(:username, :password, :remember_me)}
     devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name, :email, :password, :username, :password_confirmation)}
   end
-  def ensure_signup_complete
-    # Ensure we don't go into an infinite loop
-    return if action_name == 'finish_signup'
 
-    # Redirect to the 'finish_signup' page if the user
-    # email hasn't been verified yet
+  def ensure_signup_complete
+    return if action_name == 'finish_signup'
     if current_user && !current_user.email_verified?
       redirect_to finish_signup_path(current_user)
     end
   end
+# :nocov:
 end
