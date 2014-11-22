43c975ef8696257fb3bf4990faefe400d10bc9d0
João Paulo <jploko@gmail.com>
2014-11-22 10:37:37 -0200

    Implementando votação das proposições no quiz.
    Signed-off-by: Eduardo Vital <vitaldu@gmail.com>
    Signed-off-by: João Paulo <jploko@gmail.com>

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -1,5 +1,4 @@
 class QuizController < ApplicationController
-
   def index
   end
 
@@ -7,10 +6,18 @@ class QuizController < ApplicationController
     begin
       @proposition = randomize_propositions(Proposition.all)[0]
     end while @proposition.explanation.size <= 5
+
+    if (params[:vote] && params[:vote].require(:approved?) != "Pular")
+      Vote.create(vote_params)
+    end
   end
 
   def randomize_propositions(array_propositions)
     array_propositions.sort_by { rand }
   end
 
+  private
+    def vote_params
+      params.require(:vote).permit(:user_id, :proposition_id, :approved?)
+    end
 end
