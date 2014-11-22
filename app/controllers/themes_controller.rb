59fc4c8ccc2af68b48615ab96b2adeede8953e92
Wilton Rodrigues <braynwilton@gmail.com>
2014-11-22 10:35:53 -0200

    Ordenando os temas na Controller de Themes

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,11 +1,12 @@
 class ThemesController < ApplicationController
 	def index
 		@themes =  Theme.all
-		order_themes(@themes)
+		@themes = order_themes(Theme.all)
 	end
 
 	def order_themes(themes)
 		themes  = themes.to_a
 		themes.sort! {|b,a| a.propositions.count <=> b.propositions.count}
+		themes
 	end
 end
