0034f99a2810316bba394dd6cd6de0a80ec005b4
jploko <jploko@gmail.com>
2014-11-25 16:18:51 -0200

    Criando a helper para interest_profile. Signed-off-by: Eduardo Vital <vitaldu@gmail.com> Signed-off-by: João Paulo <jploko@gmail.com>

---
+++ app/helpers/interest_profile_helper.rb
@@ -0,0 +1,13 @@
+module InterestProfileHelper
+  def sort_most_approved_parllamentarian(interest_array)
+  end
+  
+  def sort_most_approved_theme(interest_array)
+  end
+  
+  def sort_most_disapproved_parllamentarian(interest_array)
+  end
+
+  def sort_most_disapproved_theme(interest_array)
+  end
+end
