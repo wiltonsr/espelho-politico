0fdea320b13e3cb198f9134324fc477f4bb85e6a
João Paulo <jploko@gmail.com>
2014-11-25 21:34:25 -0200

    Refatorando código e escrevendo metodos para o Perfil de Interesse Métodos alterados na Helper e Controller do Perfil de interesse.
    Signed-off-by: Eduardo Vital <vitaldu@gmail.com>
    Signed-off-by: João Paulo <jploko@gmail.com>

--- app/helpers/interest_profile_helper.rb
+++ app/helpers/interest_profile_helper.rb
@@ -1,13 +1,17 @@
 module InterestProfileHelper
-  def sort_most_approved_parllamentarian(interest_array)
+  def sort_most_approved_parllamentarian(interest)
+    interest = interest.where(approved?: true).group(:parliamentarian_id).count
+    interest.max_by{|id,count| count}.first
   end
   
-  def sort_most_approved_theme(interest_array)
+  def sort_most_approved_theme(interest)
   end
   
-  def sort_most_disapproved_parllamentarian(interest_array)
+  def sort_most_disapproved_parllamentarian(interest)
+    interest = interest.where(approved?: false).group(:parliamentarian_id).count
+    interest.max_by{|id,count| count}.first
   end
 
-  def sort_most_disapproved_theme(interest_array)
+  def sort_most_disapproved_theme(interest)
   end
 end
