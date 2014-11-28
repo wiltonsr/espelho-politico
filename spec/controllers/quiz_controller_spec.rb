a3306cb5cf1b19462cfbb0d420845a3c39dc5d69
Eduardo Vital <vitaldu@gmail.com>
2014-11-28 00:05:19 -0200

    Acrescentando mais testes para a controller do quiz.

--- spec/controllers/quiz_controller_spec.rb
+++ spec/controllers/quiz_controller_spec.rb
@@ -1,12 +1,50 @@
 require 'rails_helper'
+include Devise::TestHelpers
 
 RSpec.describe QuizController, :type => :controller do
   before(:each) do
+    create(:parliamentarian, id: 1, name: "Eduardo")
+    create(:parliamentarian, id: 2, name: "Carlos")
+    create(:parliamentarian, id: 3, name: "João")
     create(:theme, id: 1, description: "Segurança")
-    for i in 1..5 do
-      create(:proposition, id: i)
+    for i in 1..3 do
+      create(:proposition, id: i, parliamentarian_id: 1)
       Proposition.find(i).themes << Theme.find_by(description: "Segurança")
     end
+
+    create(:theme, id: 2, description: "Educação")
+    for i in 4..6 do
+      create(:proposition, id: i, parliamentarian_id: 2)
+      Proposition.find(i).themes << Theme.find_by(description: "Educação")
+    end
+
+    create(:theme, id: 3, description: "Saúde")
+    for i in 7..9 do
+      create(:proposition, id: i, parliamentarian_id: 1)
+      Proposition.find(i).themes << Theme.find_by(description: "Saúde")
+    end
+    for i in 10..13 do
+      create(:proposition, id: i, parliamentarian_id: 2)
+      Proposition.find(i).themes << Theme.find_by(description: "Saúde")
+    end
+    for i in 14..15 do
+      create(:proposition, id: i, parliamentarian_id: 3)
+      Proposition.find(i).themes << Theme.find_by(description: "Saúde")
+    end
+    user = create(:user, id: 1)
+
+    create(:vote, user_id: 1, parliamentarian_id: 1, proposition_id: 1)
+    create(:vote, user_id: 1, parliamentarian_id: 1, proposition_id: 2)
+    create(:vote, user_id: 1, parliamentarian_id: 1, proposition_id: 3)
+
+    create(:vote, user_id: 1, parliamentarian_id: 2, proposition_id: 4, approved?: false)
+    create(:vote, user_id: 1, parliamentarian_id: 2, proposition_id: 5, approved?: false)
+    create(:vote, user_id: 1, parliamentarian_id: 2, proposition_id: 6, approved?: false)
+
+    create(:vote, user_id: 1, parliamentarian_id: 1, proposition_id: 7)
+    create(:vote, user_id: 1, parliamentarian_id: 2, proposition_id: 10)
+    create(:vote, user_id: 1, parliamentarian_id: 3, proposition_id: 14, approved?: false)
+    create(:vote, user_id: 1, parliamentarian_id: 3, proposition_id: 15, approved?: false)
   end
 
   describe "GET index" do
@@ -20,6 +58,62 @@ RSpec.describe QuizController, :type => :controller do
     end
   end
 
+  describe "POST index" do
+    it "expects to create a vote" do
+      params = {
+        vote:{
+          user_id: 1,
+          proposition_id: 8,
+          approved?: true,
+          parliamentarian_id: nil
+        }
+      }
+
+      user = FactoryGirl.create(:user, id: 2)
+      sign_in user
+
+      expect {
+        post :create, params
+      }.to change(Vote, :count).by(1)
+    end
+
+    it "expects to complain about a proposition" do
+      params = {
+        vote:{
+          user_id: 2,
+          proposition_id: 1,
+          approved?: "Reclamar",
+          parliamentarian_id: nil
+        }
+      }
+
+      user = FactoryGirl.create(:user, id: 2)
+      sign_in user
+
+      expect {
+        post :create, params
+      }.to change(Vote, :count).by(0)
+    end
+
+    it "expects to complain about a proposition" do
+      params = {
+        vote:{
+          user_id: 1,
+          proposition_id: 4,
+          approved?: "Reclamar",
+          parliamentarian_id: nil
+        }
+      }
+
+      user = FactoryGirl.create(:user, id: 2)
+      sign_in user
+
+      expect {
+        post :create, params
+      }.to change(Vote, :count).by(0)
+    end
+  end
+
   describe "#randomize_propositions" do
     it "expects to shuffle propositions" do
       propositions = controller.randomize_propositions(Proposition.all.select(:id))
@@ -28,4 +122,11 @@ RSpec.describe QuizController, :type => :controller do
     end
   end
 
+  describe "#remover_proposicoes_votadas" do
+    it "expects remove all the propositions already voted by the user" do
+      quiz = QuizController.new
+      quiz.remover_proposicoes_votadas(1)
+      expect(quiz.instance_variable_get(:@proposition_hold).count).to eq(5)
+    end
+  end
 end
