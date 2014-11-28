c4c54e17aa229e49974d18257fa033a928711a29
Eduardo Vital <vitaldu@gmail.com>
2014-11-28 00:03:56 -0200

    Removendo mailers obsoletos.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,16 +1,6 @@
 class UserMailer < ActionMailer::Base
 	default from: "espelho.politico@gmail.com"
 
-	def account_activation(user)
-		@user = user
-		mail(to: @user.email, subject: "Verificação de Conta do Espelho Político")
-	end
-
-	def password_reset(user)
-		@user = user
-		mail(to: @user.email, subject: "Redefinição de senha")
-	end
-
   def complaint_about_proposition(user, parliamentarian, proposition)
     @user = User.find(user)
     @parliamentarian = Parliamentarian.find(parliamentarian)
